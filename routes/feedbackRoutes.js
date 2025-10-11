92940f481f5b7fce08c2fad39c8d64c6e845060a
Jyoti404 <guddanmaurya2004@gmail.com>
2025-10-11 14:35:01 +0530

    feedback

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { submitFeedback } from '../controllers/feedbackController.js';
+import { submitFeedback } from '../controllers/feebackController.js';
 
 const router = express.Router();
 
